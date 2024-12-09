ef9320b2aafba5a38254e820cce2dd986b5be621
bhxngu <178931769+bhxngu@users.noreply.github.com>
2024-12-08 18:16:12 -0700

    Adds url extraction

--- server.go
+++ server.go
@@ -4,15 +4,33 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
-func main() {
-	router := gin.Default()
+type RequestBody struct {
+	URL string `json:"url"`
+}
 
-	router.GET("/shorten", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+func main() {
+	ginRouter := gin.Default()
+
+	ginRouter.POST("/shorten", shorternerHandler)
+
+	ginRouter.Run(":3001")
+}
+
+func shorternerHandler(ctx *gin.Context) {
+	var body RequestBody
+	binding_type := binding.JSON
+
+	if error := ctx.ShouldBindWith(&body, binding_type); error != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": error.Error(),
 		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"url": body.URL,
 	})
-	router.Run(":3001")
 }
