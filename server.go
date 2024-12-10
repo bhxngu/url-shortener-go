92f2829a760b596175f445dda2ef8037dbd304d3
bhxngu <178931769+bhxngu@users.noreply.github.com>
2024-12-09 21:52:33 -0700

    Checks URL validity

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -9,14 +10,6 @@ import (
 
 type RequestBody struct {
 	URL string `json:"url"`
-}
-
-func main() {
-	ginRouter := gin.Default()
-
-	ginRouter.POST("/shorten", shorternerHandler)
-
-	ginRouter.Run(":3001")
 }
 
 func shorternerHandler(ctx *gin.Context) {
@@ -30,7 +23,23 @@ func shorternerHandler(ctx *gin.Context) {
 		return
 	}
 
+	u, err := url.Parse(body.URL)
+	if err != nil || u.Scheme == "" || u.Host == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid URL",
+		})
+		return
+	}
+
 	ctx.JSON(http.StatusOK, gin.H{
 		"url": body.URL,
 	})
+}
+
+func main() {
+	ginRouter := gin.Default()
+
+	ginRouter.POST("/shorten", shorternerHandler)
+
+	ginRouter.Run(":3001")
 }
